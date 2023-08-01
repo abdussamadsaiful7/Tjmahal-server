7291651829d309d6fa879ea8e1e93af7f75ea5b4
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-08-01 18:37:01 +0600

    added prisma

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
